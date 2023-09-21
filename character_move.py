ee15ad2badd5c3260826550d1cd1c14829556ff6
ldhsky <ldhsky1027@gmail.com>
2023-09-21 11:43:16 +0900

    출력 준비 완료

--- character_move.py
+++ character_move.py
@@ -3,4 +3,11 @@ open_canvas()
 
 # file here
 
+character = load_image('character.png')
+grass = load_image('grass.png')
+
+grass.draw_now(400, 30)
+character.draw_now(400,90)
+delay(1)
+
 close_canvas()
