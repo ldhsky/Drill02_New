672f3a9cf42dd51ea361c4dc14574c6e2c13464b
ldhsky <ldhsky1027@gmail.com>
2023-09-21 11:52:24 +0900

    원, 사각형 그리기 프레임 완성

--- character_move.py
+++ character_move.py
@@ -6,8 +6,23 @@ open_canvas()
 character = load_image('character.png')
 grass = load_image('grass.png')
 
+clear_canvas_now()
 grass.draw_now(400, 30)
 character.draw_now(400,90)
 delay(1)
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+    
+
+while True:
+    run_circle()
+    run_rectangle()
+
 
 close_canvas()
